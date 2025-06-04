fc476b6a04cce06b6cc52961bdb850ed3439c61d
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-04 04:56:15 -0500

    Send email

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.header("Contact Me")
+
+with st.form(key="my_forms"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("Submit")
+
+    if button:
+        message = message + user_email
---
+++ send_email.py
@@ -0,0 +1,21 @@
+import smtplib
+import ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+user_name = "tobiemanuel25@gmail.com"
+password = "htypjzdaflemqspg"
+
+receiver = "tobiemanuel25@gmail.com"
+my_context = ssl.create_default_context()
+
+message = """\
+Subject: Howdy!
+Good morning world
+"""
+
+with smtplib.SMTP_SSL(host, port, context=my_context) as server:
+    server.login(user_name, password)
+    server.sendmail(user_name, receiver, message)
+
